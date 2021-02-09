ab957e4d6963c41e11a4d48a6786a58e2e27e105
richter82 <giuseppe.baccini@live.com>
2021-02-09 10:07:25 +0100

    added .gitmodules

---
+++ checkout_contribs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd jsoncpp && git checkout 0.10.7 && cd ..
+cd spdlog && git checkout v1.8.2 && cd ..
+cd clipp && git checkout v1.2.3 && cd ..
--- get_contribs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git clone https://github.com/open-source-parsers/jsoncpp.git && cd jsoncpp && git checkout 0.10.7 && cd ..
-git clone https://github.com/gabime/spdlog.git && cd spdlog && git checkout v1.8.2 && cd ..
-git clone https://github.com/muellan/clipp.git && cd clipp && git checkout v1.2.3 && cd ..
